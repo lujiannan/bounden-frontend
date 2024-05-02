5ba68187437c7f13a1f3b762727ec59302695ce4
lujiannan <jlu2@ualberta.ca>
2024-05-02 17:10:49 -0600

    update navbar to functional component

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,66 +1,61 @@
-import { Component, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom"; // replace <a> tag with <Link> to enable routing faster (preload the page before the user clicks on the link)
-import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 import { MenuData } from "../utils/menuData";
 import "./Navbar.css";
 
-class Navbar extends Component {
-    state = {
-        clicked: false,
-        scrolled: false,
-    };
+function Navbar() {
+    const [menuClicked, setMenuClicked] = useState(false);
+    const [scrolled, setScrolled] = useState(false);
 
-    handleMenuIconClick = () => {
-        this.setState({ clicked: !this.state.clicked });
+    const handleMenuIconClick = () => {
+        setMenuClicked(!menuClicked);
     }
 
-    componentDidMount() {
-        window.addEventListener('scroll', this.handleScroll);
-    }
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+    })
 
-    handleScroll = () => {
+    const handleScroll = () => {
         // if user scrolls down > n pixels, navbar's transpareny is higher
         if (window.scrollY > 50) {
-            this.setState({ scrolled: true });
+            setScrolled(true);
         } else {
-            this.setState({ scrolled: false });
+            setScrolled(false);
         }
     }
 
     // hide the side bar when user clicks on a link (mobile)
-    handleLinkClick = () => {
-        this.setState({ clicked: false });
+    const handleLinkClick = () => {
+        setMenuClicked(false);
     }
 
-    render() {
-        return (
-            // after scrolling down, navbar's background color & height changes
-            <nav className={`NavbarItems ${this.state.scrolled ? "NavbarItems-affix" : ""}`}>
-                <h1 className="logo">
-                    <i className="fab fa-react"></i> Bounden
-                </h1>
-                <div className="menu-icon" onClick={this.handleMenuIconClick}>
-                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
-                </div>
-                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-                    {MenuData.map((item, index) => {
-                        return (
-                            <li key={index}>
-                                <Link to={item.url} className={item.cName} onClick={this.handleLinkClick}>
-                                    <i className={item.icon}></i> {item.title}
-                                </Link>
-                            </li>
-                        )
-                    })}
-                </ul>
-                <div className="user-icon">
-                    <Link to= "/login" className="nav-link-user" >
-                        <i className="fa-solid fa-user"></i>
-                    </Link>
-                </div>
-            </nav>
-        );
-    }
+    return (
+        // after scrolling down, navbar's background color & height changes
+        <nav className={`NavbarItems ${scrolled ? "NavbarItems-affix" : ""}`}>
+            <h1 className="logo">
+                <i className="fab fa-react"></i> Bounden
+            </h1>
+            <div className="menu-icon" onClick={handleMenuIconClick}>
+                <i className={menuClicked ? "fas fa-times" : "fas fa-bars"}></i>
+            </div>
+            <ul className={menuClicked ? "nav-menu active" : "nav-menu"}>
+                {MenuData.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            <Link to={item.url} className={item.cName} onClick={handleLinkClick}>
+                                <i className={item.icon}></i> {item.title}
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div className="user-icon">
+                <Link to= "/login" className="nav-link-user" >
+                    <i className="fa-solid fa-user"></i>
+                </Link>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
