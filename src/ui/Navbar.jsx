d77fddf04b15f85223da309d4d8d7e8608972f67
Jonas Lu <nannan001204@gmail.com>
2024-05-12 03:01:46 -0600

    Update Navbar.jsx

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -56,7 +56,7 @@ function Navbar() {
                 })}
             </ul>
             <div className="user-icon">
-                <Link to="/login" className={`nav-link-user ${isAuthenticated ? "active" : ""}`} id="nav-link-user">
+                <Link to="/login" className={`nav-link-user ${isAuthenticated ? "active" : ""}`}>
                     <i className="ri-user-fill"></i>
                 </Link>
             </div>
