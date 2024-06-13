2e98535b87d95c8eba82ee4679e9820d59c04858
Jonas Lu <jlu2@ualberta.ca>
2024-06-12 18:08:08 -0600

    reset btn for the generator

--- src/ui/widget/widgets/MealRecipeGenerator.jsx
+++ src/ui/widget/widgets/MealRecipeGenerator.jsx
@@ -42,69 +42,71 @@ const englishOrChinese = (str) => {
 }
 
 function MealRecipeGenerator() {
+    const defaultDishList = [
+        "馄饨",
+        "烩面",
+        "热干面",
+        "刀削面",
+        "油泼面",
+        "炸酱面",
+        "炒面",
+        "重庆小面",
+        "米线",
+        "酸辣粉",
+        "土豆粉",
+        "螺狮粉",
+        "凉皮儿",
+        "麻辣烫",
+        "肉夹馍",
+        "羊肉泡馍",
+        "炒饭",
+        "盖浇饭",
+        "烤肉饭",
+        "黄焖鸡米饭",
+        "麻辣香锅",
+        "火锅",
+        "酸菜鱼",
+        "烤串",
+        "披萨",
+        "烤鸭",
+        "汉堡",
+        "炸鸡",
+        "寿司",
+        "煎饼果子",
+        "南瓜粥",
+        "小龙虾",
+        "牛排",
+        "砂锅",
+        "大排档",
+        "馒头",
+        "西餐",
+        "自助餐",
+        "小笼包",
+        "水果",
+        "西北风",
+        "烧烤",
+        "泡面",
+        "水饺",
+        "日本料理",
+        "涮羊肉",
+        "兰州拉面",
+        "肯德基",
+        "面包",
+        "臊子面",
+        "小笼包",
+        "麦当劳",
+        "沙县小吃",
+        "烤鱼",
+        "海鲜",
+        "铁板烧",
+        "韩国料理",
+        "甜点",
+        "鸭血粉丝汤"
+    ];
+
     const [dishList, setDishList] = useState(localStorage.getItem("preferredDishMenu") ?
         localStorage.getItem("preferredDishMenu").split(/[^a-zA-Z0-9\u4e00-\u9fff ()（）]+/).filter(Boolean) :
-        [
-            "馄饨",
-            "烩面",
-            "热干面",
-            "刀削面",
-            "油泼面",
-            "炸酱面",
-            "炒面",
-            "重庆小面",
-            "米线",
-            "酸辣粉",
-            "土豆粉",
-            "螺狮粉",
-            "凉皮儿",
-            "麻辣烫",
-            "肉夹馍",
-            "羊肉泡馍",
-            "炒饭",
-            "盖浇饭",
-            "烤肉饭",
-            "黄焖鸡米饭",
-            "麻辣香锅",
-            "火锅",
-            "酸菜鱼",
-            "烤串",
-            "披萨",
-            "烤鸭",
-            "汉堡",
-            "炸鸡",
-            "寿司",
-            "煎饼果子",
-            "南瓜粥",
-            "小龙虾",
-            "牛排",
-            "砂锅",
-            "大排档",
-            "馒头",
-            "西餐",
-            "自助餐",
-            "小笼包",
-            "水果",
-            "西北风",
-            "烧烤",
-            "泡面",
-            "水饺",
-            "日本料理",
-            "涮羊肉",
-            "兰州拉面",
-            "肯德基",
-            "面包",
-            "臊子面",
-            "小笼包",
-            "麦当劳",
-            "沙县小吃",
-            "烤鱼",
-            "海鲜",
-            "铁板烧",
-            "韩国料理",
-            "甜点",
-            "鸭血粉丝汤"
-        ]
+        defaultDishList
     );
     const [dishListInputString, setDishListInputString] = useState(localStorage.getItem("preferredDishMenu") ?
         localStorage.getItem("preferredDishMenu") :
@@ -118,7 +120,6 @@ function MealRecipeGenerator() {
 
     const [shouldRunningAlertOn, setShouldRunningAlertOn] = useState(false);
     const [isCustomizeModalActive, setIsCustomizeModalActive] = useState(false);
-    const [isCustomizeModalInputSubmitting, setIsCustomizeModalInputSubmitting] = useState(false);
 
     const intervalRef = useRef(null);
 
@@ -195,12 +196,14 @@ function MealRecipeGenerator() {
                 <div className='customize-dish-menu-modal-container'>
                     <form onSubmit={handleCustomizeModalSubmit}>
                         <textarea placeholder="What's your flavor?" value={dishListInputString} onChange={(e) => { setDishListInputString(e.target.value); }} />
-                        <button type="submit">
-                            {isCustomizeModalInputSubmitting ?
-                                <div className="customize-input-submitting-container">
-                                    <div className="loading-pulse"></div>
-                                </div> : "Save"}
-                        </button>
+                        <div className='horizontal-btn-group'>
+                            <button type="submit">
+                                Save
+                            </button>
+                            <button type="button" onClick={() => {setDishListInputString(defaultDishList.join("/")); }}>
+                                Reset
+                            </button>
+                        </div>
                     </form>
                 </div>
             </FullModal>
