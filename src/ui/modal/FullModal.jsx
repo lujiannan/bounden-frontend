c780ad499214af1523636f411b575e45b90673a3
Jonas Lu <jlu2@ualberta.ca>
2024-06-19 18:58:39 -0600

    update

--- src/ui/modal/FullModal.jsx
+++ src/ui/modal/FullModal.jsx
@@ -2,35 +2,36 @@ import React, { useEffect } from 'react';
 
 import './FullModal.css'
 
-const FullModal = ({ isOpen=false, onClose, children }) => {
-  useEffect(() => {
-    const handleEscape = (event) => {
-      if (event.key === 'Escape') {
-        onClose();
-      }
-    };
+const FullModal = ({ isOpen = false, onClose, children }) => {
+    useEffect(() => {
+        const handleEscape = (event) => {
+            if (event.key === 'Escape') {
+                onClose();
+            }
+        };
 
-    if (isOpen) {
-      document.addEventListener('keydown', handleEscape);
-    }
+        if (isOpen) {
+            document.addEventListener('keydown', handleEscape);
+        }
 
-    return () => {
-      document.removeEventListener('keydown', handleEscape);
-    };
-  }, [isOpen, onClose]);
+        return () => {
+            document.removeEventListener('keydown', handleEscape);
+        };
+    }, [isOpen, onClose]);
 
-//   if (!isOpen) return null;
+    //   if (!isOpen) return null;
 
-  return (
-    <div className={`full-modal ${isOpen? 'open' : ''}`}>
-      <div className="full-modal-content">
-        <span className="full-modal-close" onClick={onClose}>
-            <i className="ri-close-line"></i>
-        </span>
-        {children}
-      </div>
-    </div>
-  );
+    return (
+        <div className={`full-modal ${isOpen ? 'open' : ''}`}>
+            <div className='full-modal-bg' onClick={onClose}></div>
+            <div className="full-modal-content">
+                <span className="full-modal-close" onClick={onClose}>
+                    <i className="ri-close-line"></i>
+                </span>
+                {children}
+            </div>
+        </div>
+    );
 };
 
 export default FullModal;
